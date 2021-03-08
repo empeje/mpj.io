f850aa083ae2b3d260d3618f0f6cb1c6ef257672
mpj <11813607+empeje@users.noreply.github.com>
2021-03-08 12:37:42 +0700

    Add Publications

--- src/Main.elm
+++ src/Main.elm
@@ -40,6 +40,7 @@ view model =
     div [ class "container content" ]
         [ viewHeader
         , viewBlogs
+        , viewPublications
         , viewTalks
         , viewCompanies
         , viewFooter
@@ -67,6 +68,24 @@ viewBlogs =
     List.append
         [ h2 [] [ text "Blogs" ]
         , p [] [ text "I write blogs on various platforms, here are links to places where I write tech stuff." ]
+        ]
+        (viewList data)
+        |> div []
+
+
+viewPublications : Html Msg
+viewPublications =
+    let
+        data =
+            [ ( "https://engj.org/index.php/ej/article/view/3432", "2020. Engineering Journal. Design of an Acoustic-Based Nondestructive Test (NDT) Instrument to Predict the Modulus of Elasticity of Wood" )
+            , ( "http://etd.repository.ugm.ac.id/home/detail_pencarian/111249", "2017. Undergraduate Thesis Universitas Gadjah Mada. Design and Implementation of Nondestructive Evaluation Instrument for Acoustics-based Prediction of Wood Modulus of Elasticity and a Method to Assess the Recorded Longitudinal Stress Wave" )
+            , ( "https://ieeexplore.ieee.org/document/7863250", "2016. International Conference on Information Technology and Electrical Engineering Yogyakarta. Study of Raspberry Pi 2 Quad-core Cortex-A7 CPU Cluster as a Mini Supercomputer" )
+            , ( "https://arxiv.org/abs/1612.08560", "2015. International Conference on Science and Technology Yogyakarta. PDM (Probes Distance Meter), Distance Measuring Instrument by Using Ultrasonic Propagation between Probes" )
+            ]
+    in
+    List.append
+        [ h2 [] [ text "Publications" ]
+        , p [] [ text "I spent almost 5 years in college and it has a big influence on me. I learn how to learn solely for curiosity and it turned out resulting several research publications. Here are my past publications as I don't write any new research publications anymore." ]
         ]
         (viewList data)
         |> div []
